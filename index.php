bf7c67e8bf11f7a4c035bd39f10f161718e298a4
Ayur Gonchikov <gonchikov@inbox.ru>
2022-02-27 20:24:24 +0800

    lesson9

--- index.php
+++ index.php
@@ -1,148 +1,81 @@
 <?php
-namespace Ayur\Pattern\Observer;
+namespace Ayur\Sort;
 
 /**
- * PHP имеет несколько встроенных интерфейсов, связанных с паттерном
- * Наблюдатель.
- *
- * Вот как выглядит интерфейс Издателя:
- *
- * @link http://php.net/manual/ru/class.splsubject.php
- *
- *     interface SplSubject
- *     {
- *         // Присоединяет наблюдателя к издателю.
- *         public function attach(SplObserver $observer);
- *
- *         // Отсоединяет наблюдателя от издателя.
- *         public function detach(SplObserver $observer);
- *
- *         // Уведомляет всех наблюдателей о событии.
- *         public function notify();
- *     }
- *
- * Также имеется встроенный интерфейс для Наблюдателей:
- *
- * @link http://php.net/manual/ru/class.splobserver.php
- *
- *     interface SplObserver
- *     {
- *         public function update(SplSubject $subject);
- *     }
+ * Сортировка пузырьком это очень долго у меня прошло полчаса и сортировка была не завершена
+ * потом я прекратил этот процесс.
+ * 
+ * Шейкерная сортировка
  */
 
-/**
- * Издатель владеет некоторым важным состоянием и оповещает наблюдателей о его
- * изменениях.
- */
-class Subject implements \SplSubject
-{
-    /**
-     * @var int Для удобства в этой переменной хранится состояние Издателя,
-     * необходимое всем подписчикам.
-     */
-    public $state;
-
-    /**
-     * @var \SplObjectStorage Список подписчиков. В реальной жизни список
-     * подписчиков может храниться в более подробном виде (классифицируется по
-     * типу события и т.д.)
-     */
-    private $observers;
-
-    public function __construct()
-    {
-
-        $this->observers = new \SplObjectStorage();
-    }
-
-    /**
-     * Методы управления подпиской.
-     */
-    public function attach(\SplObserver $observer) : void
-    {
-
-        echo "Subject: Attached an observer.\n";
-        $this->observers->attach($observer);
-    }
-
-    public function detach(\SplObserver $observer) : void
-    {
-
-        $this->observers->detach($observer);
-        echo "Subject: Detached an observer.\n";
-    }
-
-    /**
-     * Запуск обновления в каждом подписчике.
-     */
-    public function notify() : void
-    {
-
-        echo "Subject: Notifying observers...\n";
-        foreach ($this->observers as $observer) {
-            $observer->update($this);
-        }
-    }
-
-    /**
-     * Обычно логика подписки – только часть того, что делает Издатель. Издатели
-     * часто содержат некоторую важную бизнес-логику, которая запускает метод
-     * уведомления всякий раз, когда должно произойти что-то важное (или после
-     * этого).
-     */
-    public function someBusinessLogic() : void
-    {
-
-        echo "\nSubject: I'm doing something important.\n";
-        $this->state = rand(0, 10);
-
-        echo "Subject: My state has just changed to: {$this->state}\n";
-        $this->notify();
-    }
+$arr = array();
+// print_r($a);
+for ($i=0; $i<1000000; $i++) {
+  $num = rand(0, 1000000);
+  array_push($arr, $num);
 }
 
-/**
- * Конкретные Наблюдатели реагируют на обновления, выпущенные Издателем, к
- * которому они прикреплены.
- */
-class ConcreteObserverA implements \SplObserver
-{
-    public function update(\SplSubject $subject) : void
-    {
-
-        if ($subject->state < 3) {
-            echo "ConcreteObserverA: Reacted to the event.\n";
-        }
-    }
+function bubbleSort($array){
+  for($i=0; $i<count($array); $i++){
+$count = count($array);
+     for($j=$i+1; $j<$count; $j++){
+         if($array[$i]>$array[$j]){
+             $temp = $array[$j];
+             $array[$j] = $array[$i];
+             $array[$i] = $temp;
+         }
+    }         
+ }
+ return $array;
 }
 
-class ConcreteObserverB implements \SplObserver
-{
-    public function update(\SplSubject $subject) : void
-    {
-
-        if ($subject->state == 0 || $subject->state >= 2) {
-            echo "ConcreteObserverB: Reacted to the event.\n";
-        }
+function shakerSort ($array) {
+  $n = count($array);
+  $left = 0;
+  $right = $n - 1;
+  do {
+    for ($i = $left; $i < $right; $i++) {
+      if ($array[$i] > $array[$i + 1]) {
+      list($array[$i], $array[$i + 1]) = array($array[$i + 1], $array[$i]);
     }
+  }
+  $right -= 1;
+  for ($i = $right; $i > $left; $i--) {
+    if ($array[$i] < $array[$i - 1]) {
+      list($array[$i], $array[$i - 1]) = array($array[$i - 1], $array[$i]);
+    }
+  }
+  $left += 1;
+  } while ($left <= $right);
+}
+  
+function quickSort(&$arr, $low, $high) {
+  $i = $low;                
+  $j = $high;
+  $middle = $arr[ ( $low + $high ) / 2 ];  // middle – опорный элемент; в нашей реализации он находится посередине между low и high
+  do {
+    while($arr[$i] < $middle) ++$i;  // Ищем элементы для правой части
+      while($arr[$j] > $middle) -$j;  // Ищем элементы для левой части
+        if($i <= $j){           
+// Перебрасываем элементы
+          $temp = $arr[$i];
+          $arr[$i] = $arr[$j];
+          $arr[$j] = $temp;
+// Следующая итерация
+          $i++; $j--;
+        }
+  }
+  while($i < $j);
+  
+  if($low < $j){
+// Рекурсивно вызываем сортировку для левой части
+    quickSort($arr, $low, $j);
+  }
+
+  if($i < $high){
+// Рекурсивно вызываем сортировку для правой части
+    quickSort($arr, $i, $high);
+  }
 }
 
-/**
- * Клиентский код.
- */
-
-$subject = new Subject();
-
-$o1 = new ConcreteObserverA();
-$subject->attach($o1);
-
-$o2 = new ConcreteObserverB();
-$subject->attach($o2);
-
-$subject->someBusinessLogic();
-$subject->someBusinessLogic();
-
-$subject->detach($o2);
-
-$subject->someBusinessLogic();
+quickSort($arr, 0, 999999);
--- index2.php
+++ index2.php
@@ -1,125 +1,41 @@
 <?php
-namespace Ayur\Pattern\Adapter;
+namespace Ayur\Search;
 
-/**
- * Контекст определяет интерфейс, представляющий интерес для клиентов.
- */
-class Context
-{
-    /**
-     * @var Strategy Контекст хранит ссылку на один из объектов Стратегии.
-     * Контекст не знает конкретного класса стратегии. Он должен работать со
-     * всеми стратегиями через интерфейс Стратегии.
-     */
-    private $strategy;
-
-    /**
-     * Обычно Контекст принимает стратегию через конструктор, а также
-     * предоставляет сеттер для её изменения во время выполнения.
-     */
-    public function __construct(Strategy $strategy)
-    {
-
-        $this->strategy = $strategy;
-    }
-
-    /**
-     * Обычно Контекст позволяет заменить объект Стратегии во время выполнения.
-     */
-    public function setStrategy(Strategy $strategy)
-    {
-
-        $this->strategy = $strategy;
-    }
-
-    /**
-     * Вместо того, чтобы самостоятельно реализовывать множественные версии
-     * алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
-     */
-    public function doSomeBusinessLogic() : void
-    {
-
-        // ...
-
-        echo "Context: Sorting data using the strategy (not sure how it'll do it)\n";
-        $result = $this->strategy->doAlgorithm([
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-        ]);
-        echo implode(",", $result) . "\n";
-
-        // ...
-    }
+$arr = array();
+// print_r($a);
+for ($i=0; $i<10; $i++) {
+  $num = rand(0, 10);
+  array_push($arr, $num);
 }
 
-/**
- * Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых версий
- * некоторого алгоритма.
- *
- * Контекст использует этот интерфейс для вызова алгоритма, определённого
- * Конкретными Стратегиями.
- */
-interface Strategy
-{
-    public function doAlgorithm(array $data) : array;
-}
-
-/**
- * Конкретные Стратегии реализуют алгоритм, следуя базовому интерфейсу
- * Стратегии. Этот интерфейс делает их взаимозаменяемыми в Контексте.
- */
-class ConcreteStrategyPayQiwi implements Strategy
-{
-    public function doAlgorithm(array $data) : array
-    {
-
-        sort($data);
-
-        return $data;
+function binarySearch ($myArray, $num) {
+  //определяем границы массива
+  $left = 0;
+  $right = count($myArray) - 1;
+  while ($left <= $right) {
+  //находим центральный элемент с округлением индекса в меньшую сторону
+    $middle = floor(($right + $left)/2);
+  //если центральный элемент и есть искомый   
+    if ($myArray[$middle] == $num) {
+      return $middle;
     }
-}
-
-class ConcreteStrategyPayYandex implements Strategy
-{
-    public function doAlgorithm(array $data) : array
-    {
-
-        rsort($data);
-
-        return $data;
+    elseif ($myArray[$middle] > $num) {
+  //сдвигаем границы массива до диапазона от left до middle-1
+      $right = $middle - 1;
     }
-}
-
-class ConcreteStrategyPayWebMoney implements Strategy
-{
-    public function doAlgorithm(array $data) : array
-    {
-
-        array_push($data, "f", "g");
-
-        return $data;
+    elseif ($myArray[$middle] < $num) {
+      $left = $middle + 1;
     }
+  }
+  return null;
 }
 
-/**
- * Клиентский код выбирает конкретную стратегию и передаёт её в контекст. Клиент
- * должен знать о различиях между стратегиями, чтобы сделать правильный выбор.
- */
-$context = new Context(new ConcreteStrategyPayQiwi());
-echo "Client: Strategy is set to pay Qiwi.\n";
-$context->doSomeBusinessLogic();
+$idxSearch=true;
 
-echo "\n";
+while ($idxSearch) {
+  $idxSearch = binarySearch($arr, 6);
+  echo "idx" . $idxSearch . "\n";
+  unset($arr[$idxSearch]);
+}
 
-echo "Client: Strategy is set to pay Yandex.\n";
-$context->setStrategy(new ConcreteStrategyPayYandex());
-$context->doSomeBusinessLogic();
-
-echo "\n";
-
-echo "Client: Strategy is set to pay WebMoney.\n";
-$context->setStrategy(new ConcreteStrategyPayWebMoney());
-$context->doSomeBusinessLogic();
+print_r($arr) ;
