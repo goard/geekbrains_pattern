728e9c12f003e0b511987fdf3725ffd2ed58ccac
Ayur Gonchikov <gonchikov@inbox.ru>
2022-02-27 13:50:35 +0800

    lesson6 success 2 task

--- index2.php
+++ index2.php
@@ -1,30 +1,125 @@
 <?php
 namespace Ayur\Pattern\Adapter;
 
-/** 
- * Сложность алгоритма для индексированному массиву
- * индексация в массиве оществляется по праилам бинарного дерева
- * Сложность алгоритма для бинарного дерева равна О(logN)
+/**
+ * Контекст определяет интерфейс, представляющий интерес для клиентов.
  */
+class Context
+{
+    /**
+     * @var Strategy Контекст хранит ссылку на один из объектов Стратегии.
+     * Контекст не знает конкретного класса стратегии. Он должен работать со
+     * всеми стратегиями через интерфейс Стратегии.
+     */
+    private $strategy;
 
-/** 
- * Сложность алгоритма для дублирования массива через foreach
- * При создании дубликата массива через цикл foreach здесь мы должны
- * пройтись по каждому элементу.
- * Сложность алгоритма для цикла равна O(N)
- */
+    /**
+     * Обычно Контекст принимает стратегию через конструктор, а также
+     * предоставляет сеттер для её изменения во время выполнения.
+     */
+    public function __construct(Strategy $strategy)
+    {
+
+        $this->strategy = $strategy;
+    }
+
+    /**
+     * Обычно Контекст позволяет заменить объект Стратегии во время выполнения.
+     */
+    public function setStrategy(Strategy $strategy)
+    {
+
+        $this->strategy = $strategy;
+    }
+
+    /**
+     * Вместо того, чтобы самостоятельно реализовывать множественные версии
+     * алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
+     */
+    public function doSomeBusinessLogic() : void
+    {
+
+        // ...
+
+        echo "Context: Sorting data using the strategy (not sure how it'll do it)\n";
+        $result = $this->strategy->doAlgorithm([
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+        ]);
+        echo implode(",", $result) . "\n";
+
+        // ...
+    }
+}
 
 /**
- * Сложность алгоритма для рекурсивной функции нахождения факториала
- * Здесь идет вложенность фунций
- * Сложность алгоритма для цикла равна O(N)
+ * Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых версий
+ * некоторого алгоритма.
+ *
+ * Контекст использует этот интерфейс для вызова алгоритма, определённого
+ * Конкретными Стратегиями.
  */
-
-function factorial($number) { 
-
-  if ($number < 2) {                            // O(1)
-      return 1;                                 // O(1)
-  } else { 
-      return ($number * factorial($number-1));  // O(N)
-  } 
+interface Strategy
+{
+    public function doAlgorithm(array $data) : array;
 }
+
+/**
+ * Конкретные Стратегии реализуют алгоритм, следуя базовому интерфейсу
+ * Стратегии. Этот интерфейс делает их взаимозаменяемыми в Контексте.
+ */
+class ConcreteStrategyPayQiwi implements Strategy
+{
+    public function doAlgorithm(array $data) : array
+    {
+
+        sort($data);
+
+        return $data;
+    }
+}
+
+class ConcreteStrategyPayYandex implements Strategy
+{
+    public function doAlgorithm(array $data) : array
+    {
+
+        rsort($data);
+
+        return $data;
+    }
+}
+
+class ConcreteStrategyPayWebMoney implements Strategy
+{
+    public function doAlgorithm(array $data) : array
+    {
+
+        array_push($data, "f", "g");
+
+        return $data;
+    }
+}
+
+/**
+ * Клиентский код выбирает конкретную стратегию и передаёт её в контекст. Клиент
+ * должен знать о различиях между стратегиями, чтобы сделать правильный выбор.
+ */
+$context = new Context(new ConcreteStrategyPayQiwi());
+echo "Client: Strategy is set to pay Qiwi.\n";
+$context->doSomeBusinessLogic();
+
+echo "\n";
+
+echo "Client: Strategy is set to pay Yandex.\n";
+$context->setStrategy(new ConcreteStrategyPayYandex());
+$context->doSomeBusinessLogic();
+
+echo "\n";
+
+echo "Client: Strategy is set to pay WebMoney.\n";
+$context->setStrategy(new ConcreteStrategyPayWebMoney());
+$context->doSomeBusinessLogic();
--- index3.php
+++ index3.php
@@ -1,32 +1,154 @@
 <?php
-namespace Ayur\Pattern\Adapter;
+namespace Ayur\Pattern\Command;
 
-$n = 100;                           // O(1)
-$array[]= [];                       // O(1)
+/**
+ * Интерфейс Команды объявляет метод для выполнения команд.
+ */
+interface Command
+{
+    public function execute() : void;
+}
 
-for ($i = 0; $i < $n; $i++) {       // O(N)
-  for ($j = 1; $j < $n; $j *= 2) {  // O(N/2)
-     $array[$i][$j]= true;          // 
-} }                                 //
-//
-// O(1) + O(1) + O(N) * O(N/2)
-// O(2) + O(N^2/2)
-// O(2 + N^2/2) 
-// O(N^2/2)
-// O(N^2)
-//
+/**
+ * Некоторые команды способны выполнять простые операции самостоятельно.
+ */
+class SimpleCommand implements Command
+{
+    private $payload;
 
-$n = 100;                         // O(1)
-$array[] = [];                    // O(1)
+    public function __construct(string $payload)
+    {
 
-for ($i = 0; $i < $n; $i += 2) {  // O(N-2)
-  for ($j = $i; $j < $n; $j++) {  // O(N)
-     $array[$i][$j]= true;
-} }
+        $this->payload = $payload;
+    }
 
-//
-// O(1) + O(1) + O(N-2) * O(N)
-// O(2) + O(N^2-2N)
-// O(2 + N^2 - 2N)
-// O(N^2 - 2N)
-// 
+    public function execute() : void
+    {
+
+        echo "SimpleCommand: See, I can do simple things like printing (" . $this->payload . ")\n";
+    }
+}
+
+/**
+ * Но есть и команды, которые делегируют более сложные операции другим объектам,
+ * называемым «получателями».
+ */
+class ComplexCommand implements Command
+{
+    /**
+     * @var Receiver
+     */
+    private $receiver;
+
+    /**
+     * Данные о контексте, необходимые для запуска методов получателя.
+     */
+    private $a;
+
+    private $b;
+
+    /**
+     * Сложные команды могут принимать один или несколько объектов-получателей
+     * вместе с любыми данными о контексте через конструктор.
+     */
+    public function __construct(Receiver $receiver, string $a, string $b)
+    {
+
+        $this->receiver = $receiver;
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    /**
+     * Команды могут делегировать выполнение любым методам получателя.
+     */
+    public function execute() : void
+    {
+
+        echo "ComplexCommand: Complex stuff should be done by a receiver object.\n";
+        $this->receiver->doSomething($this->a);
+        $this->receiver->doSomethingElse($this->b);
+    }
+}
+
+/**
+ * Классы Получателей содержат некую важную бизнес-логику. Они умеют выполнять
+ * все виды операций, связанных с выполнением запроса. Фактически, любой класс
+ * может выступать Получателем.
+ */
+class Receiver
+{
+    public function doSomething(string $a) : void
+    {
+
+        echo "Receiver: Working on (" . $a . ".)\n";
+    }
+
+    public function doSomethingElse(string $b) : void
+    {
+
+        echo "Receiver: Also working on (" . $b . ".)\n";
+    }
+}
+
+/**
+ * Отправитель связан с одной или несколькими командами. Он отправляет запрос
+ * команде.
+ */
+class Invoker
+{
+    /**
+     * @var Command
+     */
+    private $onStart;
+
+    /**
+     * @var Command
+     */
+    private $onFinish;
+
+    /**
+     * Инициализация команд.
+     */
+    public function setOnStart(Command $command) : void
+    {
+
+        $this->onStart = $command;
+    }
+
+    public function setOnFinish(Command $command) : void
+    {
+
+        $this->onFinish = $command;
+    }
+
+    /**
+     * Отправитель не зависит от классов конкретных команд и получателей.
+     * Отправитель передаёт запрос получателю косвенно, выполняя команду.
+     */
+    public function doSomethingImportant() : void
+    {
+
+        echo "Invoker: Does anybody want something done before I begin?\n";
+        if ($this->onStart instanceof Command) {
+            $this->onStart->execute();
+        }
+
+        echo "Invoker: ...doing something really important...\n";
+
+        echo "Invoker: Does anybody want something done after I finish?\n";
+        if ($this->onFinish instanceof Command) {
+            $this->onFinish->execute();
+        }
+    }
+}
+
+/**
+ * Клиентский код может параметризовать отправителя любыми командами.
+ */
+$invoker = new Invoker();
+$invoker->setOnStart(new SimpleCommand("Say Hi!"));
+$receiver = new Receiver();
+$invoker->setOnFinish(new ComplexCommand($receiver, "Send email", "Save report"));
+
+$invoker->doSomethingImportant();
